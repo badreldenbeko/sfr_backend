654ec7f8a067eb8c567db264194a2e4d6a42c3bb
badrelden <badrelden@hotmail.com>
2019-04-07 04:27:23 +0200

    static

--- sfr_backend/settings.py
+++ sfr_backend/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
